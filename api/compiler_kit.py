683a71c9027028a6caa75fd7605da8948a20c4ea
ginanjarn <ginanjarn@gmail.com>
2024-03-30 15:48:53 +0700

    fix on multiple binary found in path

--- api/compiler_kit.py
+++ api/compiler_kit.py
@@ -8,7 +8,7 @@ import shlex
 import subprocess
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Iterator
+from typing import Iterator, List
 
 from .triple import (
     TargetTriple,
@@ -70,10 +70,11 @@ class Scanner:
         {"cc_name": "cl", "cxx_name": "cl", "version_switch": "/?"},
     ]
 
-    def _get_path(self, cc_name: str) -> PathStr:
+    def _get_path(self, cc_name: str) -> List[PathStr]:
         find_cmd = "where" if os.name == "nt" else "which"
         command = f"{find_cmd} {cc_name}"
-        return get_subprocess_result(command)
+        # Multiple executable may be detected.
+        return get_subprocess_result(command).splitlines()
 
     def _get_info(self, compiler_path: PathStr, version_switch: str) -> str:
         if not compiler_path:
@@ -87,8 +88,8 @@ class Scanner:
 
     def _get_cc_path(self, compiler_path: PathStr, triple: TargetTriple) -> PathStr:
         if triple.libc == "mingw":
-             gcc_path = compiler_path.replace("gcc", f"{triple.triple}-gcc")
-             if Path(gcc_path).exists():
+            gcc_path = compiler_path.replace("gcc", f"{triple.triple}-gcc")
+            if Path(gcc_path).exists():
                 return gcc_path
 
         return compiler_path
@@ -103,22 +104,21 @@ class Scanner:
         return "NMake Makefiles" if os.name == "nt" else "Unix Makefiles"
 
     def _scan(self) -> Iterator[CompilerKit]:
-        def get_compiler(target):
+        def get_compiler(target) -> Iterator[CompilerKit]:
             cc_name = target["cc_name"]
             cxx_name = target["cxx_name"]
 
-            if compiler_path := self._get_path(cc_name):
+            for compiler_path in self._get_path(cc_name):
                 info = self._get_info(compiler_path, target["version_switch"])
                 triple = self._get_triple(info)
 
                 cc_path = self._get_cc_path(compiler_path, triple)
                 cxx_path = cc_path.replace(cc_name, cxx_name)
                 generator = self._get_generator(triple)
-                return CompilerKit(cc_name, cc_path, cxx_path, generator)
+                yield CompilerKit(cc_name, cc_path, cxx_path, generator)
 
         for target in self.scan_compilers:
-            if compiler := get_compiler(target):
-                yield compiler
+            yield from get_compiler(target)
 
     def scan(self) -> Iterator[CompilerKit]:
         yield from self._scan()
