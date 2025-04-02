1c31e9daf91b411d0c15a9a5d74315907d891134
ginanjarn <ginanjarn@gmail.com>
2025-04-02 21:20:53 +0700

    restructure quickstart project builder

--- project_bootstrap.py
+++ project_bootstrap.py
@@ -1,9 +1,12 @@
 import threading
+from abc import ABC, abstractmethod
 from functools import wraps
+from pathlib import Path
 
+import sublime
 import sublime_plugin
 
-from .internal import project_bootstrap
+from .internal.project_bootstrap import Bootstrap, Project, ProjectType
 
 
 def wait_event(event: threading.Event, function_call: object):
@@ -29,77 +32,100 @@ def set_event(event: threading.Event):
     return func_wrapper
 
 
-class CmaketoolsQuickstartCommand(sublime_plugin.WindowCommand):
-    """"""
+class ProjectBuilder(ABC):
+    task = ""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.project_path = ""
-        self.project_type = ""
-        self.project_name = ""
+    def __init__(self, window: sublime.Window, project: Project) -> None:
+        self.window = window
+        self.project = project
 
-        self.event = threading.Event()
+    @abstractmethod
+    def prompt_input(self) -> bool:
+        """Prompt user input.
+        Return False if invalid.
+        """
 
-    def run(self):
-        thread = threading.Thread(target=self._run)
-        thread.start()
 
-    def _run(self):
-        self._select_project_path()
-        if not self.project_path:
-            return
+class SelectPath(ProjectBuilder):
+    task = "select path"
 
-        self._select_project_type()
-        if not self.project_type:
-            return
+    def prompt_input(self) -> bool:
+        if (path := Path(self._select_project_path())) and path.is_dir():
+            self.project.path = path
+            return True
+        return False
 
-        self._input_project_name()
-        if not self.project_name:
-            return
-
-        bootstrapper = project_bootstrap.Bootstrap(self.project_path)
-        project = bootstrapper.generate(self.project_type, self.project_name)
-
-        for file in project.files:
-            file.save()
-
-    def _select_project_path(self):
+    def _select_project_path(self) -> str:
         folders = self.window.folders()
+        event = threading.Event()
+        selected_path = ""
 
-        @set_event(self.event)
+        @set_event(event)
         def select_folder(index):
             if index < 0:
                 return
-            self.project_path = folders[index]
+
+            nonlocal selected_path
+            selected_path = folders[index]
 
         wait_event(
-            self.event,
+            event,
             self.window.show_quick_panel(folders, on_select=select_folder),
         )
+        return selected_path
 
-    def _select_project_type(self):
-        types = ["executable", "library"]
 
-        @set_event(self.event)
+class SelectType(ProjectBuilder):
+    task = "select type"
+
+    def prompt_input(self) -> bool:
+        if tp := self._select_project_type():
+            self.project.type = tp
+            return True
+        return False
+
+    def _select_project_type(self) -> str:
+        types = [t.value for t in ProjectType]
+        event = threading.Event()
+        selected_type = None
+
+        @set_event(event)
         def select_types(index):
             if index < 0:
                 return
-            self.project_type = types[index]
+
+            nonlocal selected_type
+            selected_type = types[index]
 
         wait_event(
-            self.event,
+            event,
             self.window.show_quick_panel(types, on_select=select_types),
         )
+        return selected_type
+
+
+class InputName(ProjectBuilder):
+    task = "input name"
+
+    def prompt_input(self) -> bool:
+        if name := self._input_project_name():
+            self.project.name = name
+            return True
+        return False
 
     def _input_project_name(self):
-        @set_event(self.event)
+        input_name = ""
+        event = threading.Event()
+
+        @set_event(event)
         def set_project_name(text):
             if not text:
                 return
-            self.project_name = text
+            nonlocal input_name
+            input_name = text
 
         wait_event(
-            self.event,
+            event,
             self.window.show_input_panel(
                 "Project name",
                 initial_text="awesome_project",
@@ -108,3 +134,29 @@ class CmaketoolsQuickstartCommand(sublime_plugin.WindowCommand):
                 on_cancel=None,
             ),
         )
+        return input_name
+
+
+class CmaketoolsQuickstartCommand(sublime_plugin.WindowCommand):
+    """"""
+
+    def run(self):
+        thread = threading.Thread(target=self._run)
+        thread.start()
+
+    def _run(self):
+        project = Project("", None, None, None)
+
+        input_flow = [SelectPath, SelectType, InputName]
+        for cls in input_flow:
+            prompt = cls(self.window, project)
+            ok = prompt.prompt_input()
+            if not ok:
+                print(f"task incomplete: {prompt.task}")
+                return
+
+        bootstrapper = Bootstrap(project.path)
+        project = bootstrapper.generate(project.type, project.name)
+
+        for file in project.files:
+            file.save()
