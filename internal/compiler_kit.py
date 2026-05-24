53414a6e68a3830b8d6cbe2f024eaa0422237d8e
ginanjarn <ginanjarn@gmail.com>
2026-05-24 11:59:46 +0700

    improve compiler_kit

--- internal/compiler_kit.py
+++ internal/compiler_kit.py
@@ -7,7 +7,6 @@ import os
 import shutil
 from dataclasses import dataclass
 from io import StringIO
-from pathlib import Path
 from typing import Iterator, List
 
 from .subprocess_helper import exec_subprocess, CaptureOption
@@ -45,33 +44,33 @@ class CompilerKitScanner:
     ]
 
     def _scan(self) -> Iterator[CompilerKit]:
-
         for target in self.scan_target:
-            yield from self._get_compiler_kit(target)
+            if kit := self._get_compiler_kit(target):
+                yield kit
 
-    def _get_compiler_kit(self, compiler_target: dict) -> Iterator[CompilerKit]:
+    def _get_compiler_kit(self, compiler_target: dict) -> CompilerKit:
         c_compiler = compiler_target["c_compiler"]
         cxx_compiler = compiler_target["cxx_compiler"]
 
-        for path in self._get_paths(c_compiler):
-            info = self._version_info(path, compiler_target["version_switch"])
-            triple = self._get_triple(info)
+        cc_path = self._find_path(c_compiler)
+        if not cc_path:
+            return None
 
-            cc_path = self._get_cc_path(path, triple)
-            cxx_path = cc_path.replace(c_compiler, cxx_compiler)
-            generator = self._get_generator(triple)
+        info = self._version_info(cc_path, compiler_target["version_switch"])
+        triple = self._get_triple(info)
 
-            yield CompilerKit(c_compiler, cc_path, cxx_path, generator)
+        if triple.libc == "mingw":
+            cc_path = cc_path.replace("gcc", f"{triple.triple}-gcc")
 
-    def _get_paths(self, executable_name: str) -> PathStr:
+        cxx_path = cc_path.replace(c_compiler, cxx_compiler)
+        generator = self._get_generator(triple)
+        return CompilerKit(c_compiler, cc_path, cxx_path, generator)
+
+    def _find_path(self, executable_name: str) -> PathStr:
         return shutil.which(executable_name, mode=os.X_OK)
 
     def _version_info(self, compiler_path: PathStr, version_switch: str) -> str:
-        if not compiler_path:
-            return ""
-
         command = [compiler_path, version_switch]
-
         writer = StringIO()
         return_code = exec_subprocess(command, writer, captures=CaptureOption.ALL)
         if return_code != 0:
@@ -81,24 +80,16 @@ class CompilerKitScanner:
     def _get_triple(self, version_info: str) -> TargetTriple:
         return TargetTripleParser(version_info).parse()
 
-    def _get_cc_path(self, compiler_path: PathStr, triple: TargetTriple) -> PathStr:
-        if triple.libc == "mingw":
-            gcc_path = compiler_path.replace("gcc", f"{triple.triple}-gcc")
-            if Path(gcc_path).is_file():
-                return gcc_path
-
-        return compiler_path
-
     def _get_generator(self, triple: TargetTriple) -> GeneratorStr:
         """generator for specific target triple"""
 
+        if self._find_path("ninja"):
+            return "Ninja"
         if triple.target_os == "msys":
             return "MSYS Makefiles"
-
         if triple.libc == "mingw":
             return "MinGW Makefiles"
-
-        return "NMake Makefiles" if os.name == "nt" else "Unix Makefiles"
+        return ""
 
 
 def scan_compilers() -> List[CompilerKit]:
